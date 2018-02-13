4ffb9c62ff8a3117308fb4652a08357003fcab5b
LuisFMendez <34488876+LuisFMendez@users.noreply.github.com>
2018-02-13 12:50:33 -0700

    Add files via upload

---
+++ Conditionals.py
@@ -0,0 +1,7 @@
+#Luis Mendez - Lesson 2 - Conditionals Challenge 1 - 02132018
+
+if 2 == 2:
+	print ("I'll print!")
+else:
+	print ("I won't print!")
+
---
+++ Conditionals2.py
@@ -0,0 +1,11 @@
+#Luis Mendez - Lesson 2 - Conditionals challenge 2 - 02132018
+
+magic_num = 5
+
+if 6 > magic_num:
+	print ("Snaller number")
+elif magic_num > 10:
+	print ("Biiiiiig number")
+else:
+	print ("Some number?")
+
